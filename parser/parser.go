7cdd69aec5bbdd9d501e921301acf47ae45b8e7a
ryo kawamura <kawa1214mura@gmail.com>
2021-12-12 20:22:21 +0900

    support: return

--- parser/parser.go
+++ parser/parser.go
@@ -52,6 +52,8 @@ func (p *Parser) parseStatement() ast.Statement {
   switch p.curToken.Type {
   case token.LET:
     return p.parseLetStatement()
+  case token.RETURN:
+    return p.parseReturnStatement()
   default:
     return nil
   }
@@ -70,6 +72,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
     return nil
   }
 
+  // TODO: セミコロンに到達するまで式を読み飛ばしてしまっている
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+  stmt := &ast.ReturnStatement{Token: p.curToken}
+
+  p.nextToken()
+
+  // TODO: セミコロンに到達するまで式を読み飛ばしてしまっている
   for !p.curTokenIs(token.SEMICOLON) {
     p.nextToken()
   }
